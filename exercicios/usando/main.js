88da85acf3a5eade3b4eb37b0d0d43433b5a5b4e
Machadomq <gbrielmq1213@gmail.com>
2024-02-29 19:25:28 -0300

    update

--- exercicios/usando/main.js
+++ exercicios/usando/main.js
@@ -3,7 +3,21 @@
 
 
 
-(function(){
-    console.log(123456)
-})();
+function criaPessoa(nome, sobrenome, a, p){
+    return {
+        nome, 
+        sobrenome,
+        fala:function (assunto){
+            return `${nome} está ${assunto}.`
+        },
+        altura:a,
+        peso:p,
+        imc(){
+            const indice = this.peso / (this.altura **2);
+            return indice 
+        }
+    };
+}
 
+const p1 = criaPessoa('luiz', 'otavio');
+console.log(p1.fala('falando'));
